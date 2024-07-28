857428a5571c185e4f8ad99aa7da7c4eb8e79f84
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-27 17:52:04 -0700

    css colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { FaGithub, FaInstagram, FaXTwitter } from "react-icons/fa6"
 export default () => {
     return (
         <>
-            <Logo className="absolute bottom-12 left-12 h-24 fill-[#594545]" />
+            <Logo className="absolute bottom-12 left-12 h-24 fill-accent" />
             <div className="absolute flex flex-col justify-between h-24 bottom-12 left-44 space-y-2">
                 <Link className="flex group" href="https://github.com/33tm">
                     <FaGithub size={24} />
