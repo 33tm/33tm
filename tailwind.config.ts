3dfea2b41ec59bae3395cc08fc76aca76bf56123
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-27 17:51:04 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,13 @@
 import type { Config } from "tailwindcss"
 
-export default { content: ["./src/**/*.tsx"] } satisfies Config
+export default { 
+    content: ["./src/**/*.tsx"],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                accent: "var(--accent)"
+            }
+        }
+    }
+} satisfies Config
