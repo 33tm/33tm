09e3bc833cde48b0a565dca0974385c87055e523
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-27 17:27:31 -0700

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "@/app/style.css"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <html lang="en">
-            <body className="m-4 h-[calc(100vh-2rem)] outline outline-4 bg-[#F3EEEA] text-[#594545]">
+            <body className={`${inter.className} m-4 h-[calc(100vh-2rem)] outline outline-4 bg-[#F3EEEA] text-[#594545]`}>
                 {children}
             </body>
         </html>
