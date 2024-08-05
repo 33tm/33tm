21756b60301ad4bbaeb948f95d858fbea96ab229
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-08-05 12:59:11 -0700

    callback

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,12 @@ export default {
     reactStrictMode: false,
     images: {
         unoptimized: true
-    }
+    },
+    redirects: () => [
+        {
+            source: "/callback/:path*",
+            destination: "http://localhost:3000/:path*",
+            permanent: false
+        }
+    ]
 }
