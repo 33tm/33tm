add378069d8cccf7645921cf35ca9d336168ec3d
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-11-09 08:49:41 -0800

    Update route.ts

--- src/app/redirect/route.ts
+++ src/app/redirect/route.ts
@@ -4,5 +4,5 @@ export const GET = (req: NextRequest) => {
     const url = req.nextUrl.searchParams.get("url")
     if (!url)
         return new Response("Missing \"url\" query parameter!", { status: 400 })
-    return NextResponse.redirect(decodeURI(url))
+    return NextResponse.redirect(Buffer.from(decodeURI(url), "base64").toString())
 }
