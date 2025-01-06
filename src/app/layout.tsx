bbb14e45c398e8c7fb66b734ce960e1460d4aac4
Leo Jeong <80350771+33tm@users.noreply.github.com>
2025-01-05 23:36:02 -0800

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <html lang="en">
-            <body className={`${inter.className} m-4 h-[calc(100vh-2rem)] outline outline-4 bg-background text-accent`}>
+            <body className={`${inter.className} m-4 h-[calc(100dvh-2rem)] outline outline-4 bg-background text-accent`}>
                 {children}
             </body>
         </html>
